be54a5b1a3d7f1ac205e42594436eaa4b6e78fe2
yujie-wang <510552624@qq.com>
2020-01-02 22:41:57 +0800

    start ch4 homework

--- ch4/useSophus/useSophus.cpp
+++ ch4/useSophus/useSophus.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
+// 因为没有install，所以include的时候是双引号
 #include "sophus/so3.h"
 #include "sophus/se3.h"
 
@@ -16,56 +17,56 @@ int main( int argc, char** argv )
     Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
     
     Sophus::SO3 SO3_R(R);               // Sophus::SO(3)可以直接从旋转矩阵构造
-    Sophus::SO3 SO3_v( 0, 0, M_PI/2 );  // 亦可从旋转向量构造，还可以这样写??可以，w=θn!
+    Sophus::SO3 SO3_v( 0, 0, M_PI/2 );  // 亦可从旋转向量构造，w=θn
     Eigen::Quaterniond q(R);            // 或者四元数
     Sophus::SO3 SO3_q( q );
     // 上述表达方式都是等价的
-    // 输出SO(3)时，以so(3)形式输出
+    // 输出SO(3)时，以so(3)形式输出，即旋转向量形式，因此就是0, 0, M_PI/2
+    // 而且默认包含了transpose，具体见下
     cout<<"SO(3) from matrix: "<<SO3_R<<endl;
     cout<<"SO(3) from vector: "<<SO3_v<<endl;
     cout<<"SO(3) from quaternion :"<<SO3_q<<endl;
     
     // 使用对数映射获得它的李代数，这里重载了log函数
     Eigen::Vector3d so3 = SO3_R.log();
-    // 这里为啥和SO3输出结果一样？？因为本质so3物理意义就是角轴，即旋转向量！！！
+    // so3物理意义就是角轴，即旋转向量
     cout<<"so3 = "<<so3.transpose()<<endl;
     // hat 为向量到反对称矩阵
     cout<<"so3 hat=\n"<<Sophus::SO3::hat(so3)<<endl;
     // 相对的，vee为反对称到向量，反映射关系~
     cout<<"so3 hat vee= "<<Sophus::SO3::vee( Sophus::SO3::hat(so3) ).transpose()<<endl; // transpose纯粹是为了输出美观一些
     
-    // TO-DO: 这个需要再好好看下
     // 增量扰动模型的更新
     Eigen::Vector3d update_so3(1e-4, 0, 0); //假设更新量为这么多
-    Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3)*SO3_R;
+    Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3)*SO3_R; //左乘
     cout<<"SO3 updated = "<<SO3_updated<<endl;
     
     /********************萌萌的分割线*****************************/
     cout<<"************我是分割线*************"<<endl;
     // 对SE(3)操作大同小异
     Eigen::Vector3d t(1,0,0);           // 沿X轴平移1
-    // 问题：系统如何辨认是用什么作为输入
     Sophus::SE3 SE3_Rt(R, t);           // 从R,t构造SE(3)
     Sophus::SE3 SE3_qt(q,t);            // 从q,t构造SE(3)
     cout<<"SE3 from R,t= "<<endl<<SE3_Rt<<endl;
     cout<<"SE3 from q,t= "<<endl<<SE3_qt<<endl;
     // 李代数se(3) 是一个六维向量，方便起见先typedef一下
     typedef Eigen::Matrix<double,6,1> Vector6d;
-    Vector6d se3 = SE3_Rt.log();
-    // 这里的平移表征形式是(0.7,-0.7,0)，看起来像是在新的坐标系下的位置了！且是逆时针旋转~
-    cout<<"se3 = "<<se3.transpose()<<endl;
+    Vector6d se3 = SE3_Rt.log(); // 取对数映射
     // 观察输出，会发现在Sophus中，se(3)的平移在前，旋转在后.
+    // 这里的平移表征rou = J^-1*t，所以不是(1,0,0)
+    // 平移表征形式是(0.785,-0.785,0)，逆时针旋转45度，乘以倍数
+    cout<<"se3 = "<<se3.transpose()<<endl;
     // 同样的，有hat和vee两个算符
     cout<<"se3 hat = "<<endl<<Sophus::SE3::hat(se3)<<endl;
     cout<<"se3 hat vee = "<<Sophus::SE3::vee( Sophus::SE3::hat(se3) ).transpose()<<endl;
     
     // 最后，演示一下更新
-    Vector6d update_se3; //更新量
-    update_se3.setZero();
-    // update_se3(0,0) = 1e-4d;
-    // 这个挺神奇的，为啥要改成float??
+    Vector6d update_se3; // 更新量
+    update_se3.setZero(); // 初始化
+    // 对于不同的compiler不一样，有的默认写f也是double型，所以就不能专门用d结尾
     update_se3(0,0) = 1e-4f;
-    Sophus::SE3 SE3_updated = Sophus::SE3::exp(update_se3)*SE3_Rt;
+    // update_se3(0,0) = 1e-4d;
+    Sophus::SE3 SE3_updated = Sophus::SE3::exp(update_se3)*SE3_Rt; // 左乘
     cout<<"SE3 updated = "<<endl<<SE3_updated.matrix()<<endl;
     
     return 0;
